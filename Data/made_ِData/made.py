2c8f370e67ec71c0a80c10c48a4d002eb8d82017
SHemarati <hasan.emarati.12652@gmail.com>
2023-08-01 20:45:01 +0330

    add new data frame

--- Data/made_ِData/made.py
+++ Data/made_ِData/made.py
@@ -1,14 +1,24 @@
 import datetime
+import numpy as np
+class Data_Frame():
+    def __init__(self):
+        self.Dataframe = ""
+        
+    def Date_Time(self):
+        # print(self.Dataframe)
+        start_date = datetime.datetime(2022, 1, 1, 0, 0, 0)
+        end_date = start_date + datetime.timedelta(days=365)
+        current_time = start_date
 
-def print_hourly_datetime_until_one_year(start_date):
-    end_date = start_date + datetime.timedelta(days=730)
-    current_time = start_date
+        while current_time < end_date:
+            self.DataFrame = current_time.strftime("%Y-%m-%d %H:%M:%S,")
+            print (self.DataFrame)
+            current_time += datetime.timedelta(hours=1)
 
-    while current_time < end_date:
-        print(current_time.strftime("%Y-%m-%d %H:%M:%S,"))
-        current_time += datetime.timedelta(hours=1)
+    def temperature(self):
+        return 
 
-# تاریخ شروع مورد نظر را مشخص کنید
-start_date = datetime.datetime(2022, 7, 17, 0, 0, 0)
-
-print_hourly_datetime_until_one_year(start_date)
+if __name__ == '__main__':
+    Run = Data_Frame()
+    Run.Date_Time()
+    # Run.temperature()
